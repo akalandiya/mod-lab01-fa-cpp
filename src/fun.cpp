bd5b1064e1e24d452a0eb08592e1cc541a3df4c3
akalandiya <100804445+akalandiya@users.noreply.github.com>
2022-03-12 15:22:19 +0300

    Создание функций

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,94 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cctype>
+#include <cmath>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    bool inWord = false;
+    bool isNum = false;
+    int wordCount = 0;
+    int i = 0;
+    while (str[i]) {
+        if (str[i] != ' ') {
+            inWord = true;
+            if (isdigit(str[i])) {
+                isNum = true;
+            }
+        }
+        else {
+            if (inWord) {
+                if (!isNum) {
+                    wordCount++;
+                }
+                else {
+                    isNum = false;
+                }
+                inWord = false;
+            }
+        }
+        i++;
+    }
+    return wordCount;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    bool inWord = false;
+    bool isBLat = false;
+    bool isFirst = false;
+    bool isSLat = false;
+    int wordCount = 0;
+    int i = 0;
+    while (str[i]) {
+        if (str[i] != ' ') {
+            inWord = true;
+            if (!isFirst) {
+                isFirst = true;
+                if (str[i] >= 'A' && str[i] <= 'Z') {
+                    isBLat = true;
+                    isSLat = true;
+                }
+            }
+            else {
+                if (isBLat) {
+                    if (!(str[i] >= 'a' && str[i] <= 'z')) {
+                        isSLat = false;
+                    }
+                }
+            }
+        }
+        else {
+            if (inWord) {
+                if (isSLat) {
+                    wordCount++;
+                }
+                isFirst = false;
+                isBLat = false;
+                inWord = false;
+            }
+        }
+        i++;
+    }
+    return wordCount;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    bool inWord = false;
+    int wordCount = 0;
+    int symCount = 0;
+    int averCount = 0;
+    int i = 0;
+    while (str[i]) {
+        if (str[i] != ' ') {
+            inWord = true;
+            symCount++;
+        }
+        else {
+            if (inWord) {
+                inWord = false;
+            }
+        }
+        i++;
+    }
+    averCount = round(symCount / wordCount);
+    return averCount;
 }
